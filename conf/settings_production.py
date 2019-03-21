0763544b3d6532b73b3e209c58d073023826eff5
unknown <GW00174898@CORP.GWM.COM>
2019-03-21 17:30:35 +0800

    update

--- conf/settings_production.py
+++ conf/settings_production.py
@@ -21,7 +21,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
         'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
         'USER': 'root',                            # 你的数据库user
-        'PASSWORD': 'root',                        # 你的数据库password
+        'PASSWORD': 'L*vIW=.lTr',                        # 你的数据库password
         'HOST': '10.255.30.119',                   		   # 数据库HOST
         'PORT': '3306',                        # 默认3306
     },
